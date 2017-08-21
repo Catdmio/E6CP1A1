ebd97ca1e36f3304510dd457d332decef722ca4e
Catdmio <montecinos.catalina@gmail.com>
2017-08-21 20:12:50 -0300

    added exercise 4 its a method that asks for password, which is -gatito.

--- 1 if and else/ejercicio4.rb
+++ 1 if and else/ejercicio4.rb
@@ -1,8 +1,18 @@
 # Permitir que el usuario pueda ingresar la contraseña por teclado 
 # para que se cumpla la condición.
 
-if password == 'secreto'
-  puts 'Acceso PERMITIDO! :)'
-else
-  puts 'Acceso DENEGADO! :('
+#if password == 'secreto'
+ # puts 'Acceso PERMITIDO! :)'
+#else
+ # puts 'Acceso DENEGADO! :('
+#end
+
+puts 'ingrese la clave'
+clave = gets.chomp
+
+while clave != 'gatito'
+	puts 'Accesso Denegado, intente otra vez'
+	clave = gets.chomp
 end
+
+puts '¡Éxito! Acceso permitido'
